4c165516c3bf555e4c4ac991030d03595e6d3e2a
Saparbekov Nurdan <113797353+OVERDOZZZE@users.noreply.github.com>
2024-03-20 05:46:57 +0600

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', 'localhost', 'ssnchat.vercel.app']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
