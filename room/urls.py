a7e8b6594f23b70cd9600bfbb460f158236c958c
OVERDOZZZE <saparbekov113@gmail.com>
2024-03-20 04:34:12 +0600

    Add room creation functionality

--- room/urls.py
+++ room/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.rooms, name='rooms'),
-    path('<slug:slug>/', views.room, name='room'),
+    path('/chats/<slug:slug>/', views.room, name='room'),
+    path('create/', views.create_room, name='create_room'),
 ]
