c36c1b394a6837a49573720d658718763a9f5224
Saparbekov Nurdan <113797353+OVERDOZZZE@users.noreply.github.com>
2024-03-20 06:12:08 +0600

    Update urls.py

--- room/urls.py
+++ room/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.rooms, name='rooms'),
-    path('/chats/<slug:slug>/', views.room, name='room'),
+    path('chats/<slug:slug>/', views.room, name='room'),
     path('create/', views.create_room, name='create_room'),
 ]
