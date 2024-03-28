65ca7b41c920a13b0744cb5d886a7b6782984b39
OVERDOZZZE <saparbekov113@gmail.com>
2024-03-29 02:54:00 +0600

    commit

--- core/schema.py
+++ core/schema.py
@@ -1,16 +1,92 @@
 import graphene
 from graphene_django import DjangoObjectType
-from room.models import Room
+from room.models import Room, Message
+from graphene import Enum
+
+
+class SortDirection(Enum):
+    ASC = "ASC"
+    DESC = "DESC"
+
+
+class MessageType(DjangoObjectType):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
 
 class RoomType(DjangoObjectType):
     class Meta:
         model = Room
-        field = ['id', 'name']
+        fields = ['id', 'name', 'slug']
+
 
 class Query(graphene.ObjectType):
+    list_message = graphene.List(
+        MessageType, sortBy=graphene.String(),
+        sortOrder=SortDirection()
+    )
+
     list_room = graphene.List(RoomType)
+    read_room = graphene.Field(RoomType, id=graphene.Int())
 
     def resolve_list_room(root, info):
         return Room.objects.all()
 
-schema = graphene.Schema(query=Query)
+    def resolve_list_message(root, info, sortBy=None, sortOrder=None, **kwargs):
+        queryset = Message.objects.all()
+
+        if sortBy:
+            if sortOrder == SortDirection.ASC:
+                queryset = queryset.order_by(sortBy)
+            elif sortOrder == SortDirection.DESC:
+                queryset = queryset.order_by('-'+sortBy)
+
+        return queryset
+
+    def resolve_read_room(root, info, id):
+        return Room.objects.get(id=id)
+
+
+class RoomMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String()
+        slug = graphene.String()
+
+    room = graphene.Field(RoomType)
+
+    @classmethod
+    def mutate(cls, root, info, name, slug, id=None):
+        if id:
+            room = Room.objects.get(id=id)
+            room.name = name
+            room.slug = slug
+            room.save()
+        else:
+            room = Room(name=name, slug=slug)
+            room.save()
+
+        return RoomMutation(room=room)
+
+
+class RoomDelete(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    room = graphene.Field(RoomType)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        room = Room.objects.get(id=id)
+        room.delete()
+        return RoomDelete(room=None)
+
+
+class Mutation(graphene.ObjectType):
+    create_room = RoomMutation.Field()
+    update_room = RoomMutation.Field()
+    delete_room = RoomDelete.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- room/models.py
+++ room/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
